ed72f513570d056fd452468924fee27bfa5851c6
Ryan Acham <ryanacham@hotmail.com>
2022-05-31 13:16:37 -0400

    Created empty functions

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+var key = '265c9d125eac2d1c7c7e7b9b8dc79e0d';
+var city = "Toronto"
+
+//time and date
+var date = moment().format('dddd, MMMM Do YYYY');
+var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
+//to do: button creator 
+var hist = $('.cityHist');
+function getHis() {
+	 
+};
+var cityHist = [];
+//to do: save text as array on search click
+$('.search').on("click", function (event) {
+	
+});
+//to do: get weather data for today and put it in today card
+function getWeath(){
+
+}
+//to do: get forecast for the week
+function forecast(){
+
+}
+//to do: function for first data that the user sees when the website loads
+function initCast(){
+    
+}
+initCast();
