ce00743a55e6646758c2c91f88ebdc0f40bc1e91
Ryan Acham <ryanacham@hotmail.com>
2022-05-31 20:49:20 -0400

    made search button creator

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,43 @@ var city = "Toronto"
 var date = moment().format('dddd, MMMM Do YYYY');
 var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
 //to do: button creator 
+var cityHist = [];
 var hist = $('.citHit');
 function getHis() {
-	 
+    hist.empty();
+
+	for (let i = 0; i < cityHist.length; i++) {
+
+		var rowEle = $('<row>');
+		var btnEle = $('<button>').text(`${cityHist[i]}`)
+
+		rowEle.addClass('row histBtnRow');
+		btnEle.addClass('btn btn-outline-secondary histBtn');
+		btnEle.attr('type', 'button');
+
+		hist.prepend(rowEle);
+		rowEle.append(btnEle);
+	} if (!city) {
+		return;
+	}
+	//start the search once button is clicked
+	$('.histBtn').on("click", function (event) {
+		event.preventDefault();
+		city = $(this).text();
+		forecast.empty();
+		getWeath();
+	});
 };
-var cityHist = [];
+//to do: get weather data for today and put it in today card
+function getWeath(){
+
+}
+//to do: get forecast for the week
+var forcastVal = $('forecast');
+function forecast(){
+
+}
+
 //to do: save text as array on search click
 $('.search').on("click", function (event) {
 	event.preventDefault();
@@ -24,15 +56,6 @@ $('.search').on("click", function (event) {
 	getHis();
 	getWeath();
 });
-//to do: get weather data for today and put it in today card
-function getWeath(){
-
-}
-//to do: get forecast for the week
-var forcastVal = $('forecast');
-function forecast(){
-
-}
 //to do: function for first data that the user sees when the website loads
 function initCast(){
     
