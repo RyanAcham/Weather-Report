94be141bca50414a830d6eca00369953220b96ad
Ryan Acham <ryanacham@hotmail.com>
2022-05-31 21:49:27 -0400

    added css styling and forecast function

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,7 +93,61 @@ function getWeath(){
 //to do: get forecast for the week
 var forcastVal = $('forecast');
 function forecast(){
-    
+    var getUrlfore = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${key}`;
+
+	$.ajax({
+		url: getUrlfore,
+		method: 'GET',
+	}).then(function (response) {
+		var foreArray = response.list;
+		var myWeather = [];
+        //creating objects for readability
+		$.each(foreArray, function (index, value) {
+			testObj = {
+				date: value.dt_txt.split(' ')[0],
+				time: value.dt_txt.split(' ')[1],
+				temp: value.main.temp,
+				feels_like: value.main.feels_like,
+				icon: value.weather[0].icon,
+				humidity: value.main.humidity
+			}
+
+			if (value.dt_txt.split(' ')[1] === "12:00:00") {
+				myWeather.push(testObj);
+			}
+		})
+		for (let i = 0; i < myWeather.length; i++) {
+
+			var divElCard = $('<div>');
+			divElCard.attr('class', 'card text-white bg-primary mb-3 cardOne');
+			divElCard.attr('style', 'max-width: 200px;');
+			fiveForecastEl.append(divElCard);
+
+			var divElHeader = $('<div>');
+			divElHeader.attr('class', 'card-header')
+			var m = moment(`${myWeather[i].date}`).format('MM-DD-YYYY');
+			divElHeader.text(m);
+			divElCard.append(divElHeader)
+
+			var divElBody = $('<div>');
+			divElBody.attr('class', 'card-body');
+			divElCard.append(divElBody);
+
+			var divElIcon = $('<img>');
+			divElIcon.attr('class', 'icons');
+			divElIcon.attr('src', `https://openweathermap.org/img/wn/${myWeather[i].icon}@2x.png`);
+			divElBody.append(divElIcon);
+            //temperature
+			var pElTemp = $('<p>').text(`Temperature: ${myWeather[i].temp} °F`);
+			divElBody.append(pElTemp);
+			//what it actually feels like
+			var pElFeel = $('<p>').text(`Feels Like: ${myWeather[i].feels_like} °F`);
+			divElBody.append(pElFeel);
+			//humidity
+			var pElHumid = $('<p>').text(`Humidity: ${myWeather[i].humidity} %`);
+			divElBody.append(pElHumid);
+		}
+	});
 }
 
 //to do: save text as array on search click
