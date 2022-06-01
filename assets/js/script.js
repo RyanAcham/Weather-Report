231d87874ccc306232685334dd199c3813bf2346
Ryan Acham <ryanacham@hotmail.com>
2022-05-31 20:27:09 -0400

    adding html classes and working on functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,31 @@ var city = "Toronto"
 var date = moment().format('dddd, MMMM Do YYYY');
 var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
 //to do: button creator 
-var hist = $('.cityHist');
+var hist = $('.citHit');
 function getHis() {
 	 
 };
 var cityHist = [];
 //to do: save text as array on search click
 $('.search').on("click", function (event) {
-	
+	event.preventDefault();
+	city = $(this).parent('.btnPar').siblings('.textIn').val().trim();
+	if (city === "") {
+		return;
+	};
+	cityHist.push(city);
+
+	localStorage.setItem('city', JSON.stringify(cityHist));
+	forecast.empty();
+	getHis();
+	getWeath();
 });
 //to do: get weather data for today and put it in today card
 function getWeath(){
 
 }
 //to do: get forecast for the week
+var forcastVal = $('forecast');
 function forecast(){
 
 }
